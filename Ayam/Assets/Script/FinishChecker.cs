f33260c7f4e2476a5c8e028eeb7c9df3b6ffcf67
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2018-06-12 02:45:58 +0700

    Track 2 + finish(additional)
    Next upload should be power up

--- Ayam/Assets/Script/FinishChecker.cs
+++ Ayam/Assets/Script/FinishChecker.cs
@@ -2,22 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishChecker : MonoBehaviour {
+public class FinishChecker : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameManager GameManager;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnTriggerEnter(Collider col)
     {
-        if(col.tag == "Player")
-        Debug.Log(col.name);
-       
+        if (col.tag == "Player" && col.name == "redsled")
+        {
+            GameManager.p1Move = false;
+            if (!GameManager.isWin)
+            {
+                GameManager.isWin = true;
+                GameManager.count1.text = "You Win!!!";
+            }
+            else GameManager.count1.text = "You Lose!!!";
+        }
+        else if (col.tag == "Player" && col.name == "bluesled")
+        {
+            GameManager.p2Move = false;
+            if (!GameManager.isWin)
+            {
+                GameManager.isWin = true;
+                GameManager.count2.text = "You Win!!!";
+            }
+            else GameManager.count2.text = "You Lose!!!";
+        }
     }
 }
--- Ayam/Assets/Script/GameManager.cs
+++ Ayam/Assets/Script/GameManager.cs
@@ -8,91 +8,96 @@ public class GameManager : MonoBehaviour
     public GameObject p1;
     public GameObject p2;
 
-	public Slider boost1;
-	public Slider boost2;
-	public Text count1;
-	public Text count2;
+    public Slider boost1;
+    public Slider boost2;
+    public Text count1;
+    public Text count2;
 
-	public GroundCheck gc1;
-	public GroundCheck gc2;
+    public GroundCheck gc1;
+    public GroundCheck gc2;
 
-	private Rigidbody rig1;
-	private Rigidbody rig2;
-	private float p1nitro;
-	private float p2nitro;
-	private bool isStart=false;
+    public bool isWin = false;
+    public bool p1Move = true;
+    public bool p2Move = true;
+    private Rigidbody rig1;
+    private Rigidbody rig2;
+    private float p1nitro;
+    private float p2nitro;
+    private bool isStart = false;
 
     // Use this for initialization
     void Start()
     {
-		p1nitro = p2nitro = 100f;
-		boost1.value = p1nitro;
-		boost2.value = p2nitro;
-		rig1 = p1.GetComponent<Rigidbody> ();
-		rig2 = p2.GetComponent<Rigidbody> ();
-		StartCoroutine (countdown());
+        p1nitro = p2nitro = 100f;
+        boost1.value = p1nitro;
+        boost2.value = p2nitro;
+        rig1 = p1.GetComponent<Rigidbody>();
+        rig2 = p2.GetComponent<Rigidbody>();
+        StartCoroutine(countdown());
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (isStart) {
-			rig1.constraints = RigidbodyConstraints.None;
-			rig2.constraints = RigidbodyConstraints.None;
-			rig1.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-			rig2.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-			Movementp1();
-			Movementp2();
-		}
+        if (isStart)
+        {
+            rig1.constraints = RigidbodyConstraints.None;
+            rig2.constraints = RigidbodyConstraints.None;
+            rig1.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            rig2.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            if(p1Move) Movementp1();
+            if(p2Move) Movementp2();
+        }
     }
 
     void Movementp1()
     {
         //if (p1_Win == false)
         //{
-		if (Input.GetKey(KeyCode.W) && rig1.velocity.z < 500f && gc1.gc)
-            {
-                float speed = 50f;
-                rig1.AddForce(p1.transform.forward * Time.deltaTime * speed);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                Vector3 temp = p1.transform.eulerAngles;
-                temp.y -= 45f * Time.deltaTime;
-                p1.transform.eulerAngles = temp;
-            }
-            /*else if(p1.transform.eulerAngles.y != 0f && p1.transform.eulerAngles.y>335f)
-			{
-				Vector3 temp = p1.transform.eulerAngles;
-				temp.y += 45f * Time.deltaTime;
-				p1.transform.eulerAngles = temp;
-			}*/
-		if (Input.GetKey(KeyCode.S) && rig1.velocity.z > -1f && gc1.gc)
-            {
-                rig1.velocity *= 0.9f;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                Vector3 temp = p1.transform.eulerAngles;
-                temp.y += 45f * Time.deltaTime;
-                p1.transform.eulerAngles = temp;
-            }
-            /*else if(p1.transform.eulerAngles.y != 0f && p1.transform.eulerAngles.y<25f)
-			{
-				Vector3 temp = p1.transform.eulerAngles;
-				temp.y -= 45f * Time.deltaTime;
-				p1.transform.eulerAngles = temp;
-			}*/
-			if (Input.GetKey(KeyCode.LeftShift) && p1nitro > 0)
-            {
-                rig1.AddForce(p1.transform.forward * Time.deltaTime * 100f);
-                p1nitro -= 1.5f;
-				boost1.value = p1nitro;
-            }
-			if (p1nitro < 100f) {
-				p1nitro += 0.5f;
-				boost1.value = p1nitro;
-			}
+        if (Input.GetKey(KeyCode.W) && rig1.velocity.z < 500f && gc1.gc)
+        {
+            float speed = 50f;
+            rig1.AddForce(p1.transform.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 temp = p1.transform.eulerAngles;
+            temp.y -= 45f * Time.deltaTime;
+            p1.transform.eulerAngles = temp;
+        }
+        /*else if(p1.transform.eulerAngles.y != 0f && p1.transform.eulerAngles.y>335f)
+        {
+            Vector3 temp = p1.transform.eulerAngles;
+            temp.y += 45f * Time.deltaTime;
+            p1.transform.eulerAngles = temp;
+        }*/
+        if (Input.GetKey(KeyCode.S) && rig1.velocity.z > -1f && gc1.gc)
+        {
+            rig1.velocity *= 0.9f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 temp = p1.transform.eulerAngles;
+            temp.y += 45f * Time.deltaTime;
+            p1.transform.eulerAngles = temp;
+        }
+        /*else if(p1.transform.eulerAngles.y != 0f && p1.transform.eulerAngles.y<25f)
+        {
+            Vector3 temp = p1.transform.eulerAngles;
+            temp.y -= 45f * Time.deltaTime;
+            p1.transform.eulerAngles = temp;
+        }*/
+        if (Input.GetKey(KeyCode.LeftShift) && p1nitro > 0)
+        {
+            rig1.AddForce(p1.transform.forward * Time.deltaTime * 100f);
+            p1nitro -= 1.5f;
+            boost1.value = p1nitro;
+        }
+        if (p1nitro < 100f)
+        {
+            p1nitro += 0.5f;
+            boost1.value = p1nitro;
+        }
         //}
     }
 
@@ -100,84 +105,93 @@ public class GameManager : MonoBehaviour
     {
         //if (p2_Win == false)
         //{
-		if (Input.GetKey(KeyCode.UpArrow) && rig2.velocity.z < 500f) //2nd constraint for max speed
+        if (Input.GetKey(KeyCode.UpArrow) && rig2.velocity.z < 500f) //2nd constraint for max speed
+        {
+            if (gc2.gc)
             {
-				if (gc2.gc) {
-					float speed = 50f;
-					rig2.AddForce (p2.transform.forward * Time.deltaTime * speed);
-				} else {
-					Vector3 temp = p2.transform.eulerAngles;
-					temp.x += 45f * Time.deltaTime;
-					p2.transform.eulerAngles = temp;
-				Debug.Log ("anjing");
-				}
+                float speed = 50f;
+                rig2.AddForce(p2.transform.forward * Time.deltaTime * speed);
             }
-            if (Input.GetKey(KeyCode.LeftArrow))
+            else
             {
                 Vector3 temp = p2.transform.eulerAngles;
-                temp.y -= 45f * Time.deltaTime;
+                temp.x += 45f * Time.deltaTime;
                 p2.transform.eulerAngles = temp;
+                Debug.Log("anjing");
             }
-            /*else if (p2.transform.eulerAngles.y != 0f && p2.transform.eulerAngles.y > 335f)
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 temp = p2.transform.eulerAngles;
+            temp.y -= 45f * Time.deltaTime;
+            p2.transform.eulerAngles = temp;
+        }
+        /*else if (p2.transform.eulerAngles.y != 0f && p2.transform.eulerAngles.y > 335f)
+        {
+            Vector3 temp = p2.transform.eulerAngles;
+            temp.y += 45f * Time.deltaTime;
+            p2.transform.eulerAngles = temp;
+        }*/
+        if (Input.GetKey(KeyCode.DownArrow) && rig2.velocity.z > -1f)//2nd constraint for min speed
+        {
+            if (gc2.gc)
+            {
+                rig2.velocity *= 0.9f;
+            }
+            else
             {
                 Vector3 temp = p2.transform.eulerAngles;
-                temp.y += 45f * Time.deltaTime;
+                temp.x -= 45f * Time.deltaTime;
                 p2.transform.eulerAngles = temp;
-            }*/
-		if (Input.GetKey(KeyCode.DownArrow) && rig2.velocity.z > -1f)//2nd constraint for min speed
-            {
-			if (gc2.gc) {
-				rig2.velocity *= 0.9f;
-			} else {
-				Vector3 temp = p2.transform.eulerAngles;
-				temp.x -= 45f * Time.deltaTime;
-				p2.transform.eulerAngles = temp;
-				Debug.Log ("kucing");
-			}
+                Debug.Log("kucing");
             }
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                Vector3 temp = p2.transform.eulerAngles;
-                temp.y += 45f * Time.deltaTime;
-                p2.transform.eulerAngles = temp;
-            }
-            /*else if (p2.transform.eulerAngles.y != 0f && p2.transform.eulerAngles.y < 25f)
-            {
-                Vector3 temp = p2.transform.eulerAngles;
-                temp.y -= 45f * Time.deltaTime;
-                p2.transform.eulerAngles = temp;
-            }*/
-			if (Input.GetKey(KeyCode.RightControl) && p2nitro > 0)
-			{
-				rig2.AddForce(p2.transform.forward * Time.deltaTime * 100f);
-				p2nitro -= 1.5f;
-				boost2.value = p2nitro;
-			}
-			if (p2nitro < 100f) {
-				p2nitro += 0.5f;
-				boost2.value = p2nitro;
-			}
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 temp = p2.transform.eulerAngles;
+            temp.y += 45f * Time.deltaTime;
+            p2.transform.eulerAngles = temp;
+        }
+        /*else if (p2.transform.eulerAngles.y != 0f && p2.transform.eulerAngles.y < 25f)
+        {
+            Vector3 temp = p2.transform.eulerAngles;
+            temp.y -= 45f * Time.deltaTime;
+            p2.transform.eulerAngles = temp;
+        }*/
+        if (Input.GetKey(KeyCode.RightControl) && p2nitro > 0)
+        {
+            rig2.AddForce(p2.transform.forward * Time.deltaTime * 100f);
+            p2nitro -= 1.5f;
+            boost2.value = p2nitro;
+        }
+        if (p2nitro < 100f)
+        {
+            p2nitro += 0.5f;
+            boost2.value = p2nitro;
+        }
         //}
     }
 
-	private IEnumerator countdown(){
-		float cdown = 3f;
-		float tine = 1.25f;
-		rig1.constraints = RigidbodyConstraints.FreezeAll;
-		rig2.constraints = RigidbodyConstraints.FreezeAll;
-		while (cdown>0) {
-			
-			count1.text = cdown.ToString ();
-			count2.text = cdown.ToString ();
-			cdown--;
-			yield return new WaitForSeconds (tine);
-		}
-		isStart = true;
-		count1.text = "GO!!!";
-		count2.text = "GO!!!";
-		yield return new WaitForSeconds (1.5f);
-		count1.text = "";
-		count2.text = "";
-	}
+    private IEnumerator countdown()
+    {
+        float cdown = 3f;
+        float tine = 1.25f;
+        rig1.constraints = RigidbodyConstraints.FreezeAll;
+        rig2.constraints = RigidbodyConstraints.FreezeAll;
+        while (cdown > 0)
+        {
+
+            count1.text = cdown.ToString();
+            count2.text = cdown.ToString();
+            cdown--;
+            yield return new WaitForSeconds(tine);
+        }
+        isStart = true;
+        count1.text = "GO!!!";
+        count2.text = "GO!!!";
+        yield return new WaitForSeconds(1.5f);
+        count1.text = "";
+        count2.text = "";
+    }
 
 }
