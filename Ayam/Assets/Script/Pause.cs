2f2b8b5edbb48076ec669281f5bc6b64851d548b
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2018-06-12 03:27:23 +0700

    Power Up

--- Ayam/Assets/Script/Pause.cs
+++ Ayam/Assets/Script/Pause.cs
@@ -2,35 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pause : MonoBehaviour {
+public class Pause : MonoBehaviour
+{
 
-	public GameObject pausePanel;
-	private bool ifpause = false;
+    public GameObject pausePanel;
+    private bool ifpause = false;
 
-	void Start(){
-		pausePanel.SetActive (false);
-	}
+    void Start()
+    {
+    }
 
-	void Update(){
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			ifpause = !ifpause;
-			if (ifpause) {
-				OnPause ();
-			} else {
-				NonPause ();
-			}
-		}
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ifpause = !ifpause;
+            if (ifpause)
+            {
+                OnPause();
+            }
+            else
+            {
+                NonPause();
+            }
+        }
+    }
 
-	public void OnPause(){
-		Time.timeScale = 0;
-		pausePanel.SetActive (true);
-	}
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
 
-	public void NonPause(){
-		ifpause = false;
-		pausePanel.SetActive (false);
-		Time.timeScale = 1;
-	}
-		
+    public void NonPause()
+    {
+        ifpause = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
 }
---
+++ Ayam/Assets/Script/PowerUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public GameManager GameManager;
+
+    private bool p1BoostActive;
+    private bool p2BoostActive;
+    // Use this for initialization
+    void Start()
+    {
+        p1BoostActive = false;
+        p2BoostActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.boost1.value == 0f) GameManager.boost1.gameObject.SetActive(false);
+        if (GameManager.boost2.value == 0f) GameManager.boost2.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player" && col.name == "redsled")
+        {
+            if (p1BoostActive == false) GameManager.boost1.gameObject.SetActive(true);
+            GameManager.boost1.value = 100f;
+            p1BoostActive = true;
+        }
+        else if (col.tag == "Player" && col.name == "bluesled")
+        {
+            if (p2BoostActive == false) GameManager.boost1.gameObject.SetActive(true);
+            GameManager.boost2.value = 100f;
+            p2BoostActive = true;
+        }
+    }
+}
